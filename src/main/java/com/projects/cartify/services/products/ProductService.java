fd5f240772356b8b41f3f957ba8b1012db1014ff
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2025-04-07 14:58:07 +0200

    Dealing with services

--- src/main/java/com/projects/cartify/services/products/ProductService.java
+++ src/main/java/com/projects/cartify/services/products/ProductService.java
@@ -1,4 +1,70 @@
 package com.projects.cartify.services.products;
 
-public class ProductService {
+import com.projects.cartify.exceptions.ProductNotFoundException;
+import com.projects.cartify.models.Product;
+import com.projects.cartify.repository.ProductRepository;
+
+import java.util.List;
+
+public class ProductService implements IProductService{
+
+    private ProductRepository repository;
+
+    @Override
+    public Product addProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException("Product not found"));
+    }
+
+    @Override
+    public void deleteProductById(Long id) {
+        repository.findById(id)
+                .ifPresentOrElse(repository::delete,
+                        () -> {throw new ProductNotFoundException("Product not found");});
+    }
+
+    @Override
+    public void updateProduct(Product product, Long id) {
+
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return repository.findAll();
+    }
+
+    @Override
+    public List<Product> getProductsByCategory(String category) {
+        return repository.findByCategoryName(category);
+    }
+
+    @Override
+    public List<Product> getProductsByBrand(String brand) {
+        return repository.findByBrand(brand);
+    }
+
+    @Override
+    public List<Product> getProductsByCategoryAndBrand(String category, String brand) {
+        return repository.findByCategoryNameAndBrand(category, brand);
+    }
+
+    @Override
+    public List<Product> getProductsByName(String name) {
+        return repository.findByName(name);
+    }
+
+    @Override
+    public List<Product> getProductsByBrandAndName(String brand, String name) {
+        return repository.findByBrandAndName(brand, name);
+    }
+
+    @Override
+    public Long countProductsByBrandAndName(String brand, String name) {
+        return repository.countByBrandAndName(brand, name);
+    }
 }
