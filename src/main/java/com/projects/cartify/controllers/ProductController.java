568bc8ab00131af7214aa13fd6648992d1dc5287
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2025-04-08 11:33:37 +0200

    Dealing with the controllers

--- src/main/java/com/projects/cartify/controllers/ProductController.java
+++ src/main/java/com/projects/cartify/controllers/ProductController.java
@@ -1,9 +1,58 @@
 package com.projects.cartify.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
+import com.projects.cartify.exceptions.ResourceNotFoundException;
+import com.projects.cartify.models.Product;
+import com.projects.cartify.requests.AddProductRequest;
+import com.projects.cartify.requests.ProductUpdateRequest;
+import com.projects.cartify.responses.ApiResponse;
+import com.projects.cartify.services.products.IProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("${api.prefix}/products")
 public class ProductController {
+    private final IProductService productService;
 
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllProducts()  {
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(new ApiResponse("success", products));
+    }
+
+    @GetMapping("/product/${productId}")
+    public ResponseEntity<ApiResponse> getProductById(Long id) {
+        try {
+            Product product = productService.getProductById(id);
+            return ResponseEntity.ok(new ApiResponse("success", product));
+        } catch (ResourceNotFoundException e) {
+           return ResponseEntity.status(NOT_FOUND).body(new ApiResponse("error", NOT_FOUND));
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addProduct(@RequestBody AddProductRequest request) {
+        try {
+            Product newProduct = productService.addProduct(request);
+            return ResponseEntity.ok(new ApiResponse("success", newProduct));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("error", INTERNAL_SERVER_ERROR));
+        }
+    }
+
+    public ResponseEntity<ApiResponse> updateProduct(@PathVariable Long id, @RequestBody ProductUpdateRequest request) {
+        try {
+            Product updatedProduct = productService.updateProduct(request, id);
+            return ResponseEntity.ok(new ApiResponse("success", updatedProduct));
+        } catch (Exception e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse("error", NOT_FOUND));
+        }
+    }
 }
