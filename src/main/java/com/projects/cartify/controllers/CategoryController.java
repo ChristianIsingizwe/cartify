af7148418a21e0530e62359c27317e4447e74635
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2025-04-08 09:57:20 +0200

    Dealing with the controllers

--- src/main/java/com/projects/cartify/controllers/CategoryController.java
+++ src/main/java/com/projects/cartify/controllers/CategoryController.java
@@ -1,4 +1,72 @@
 package com.projects.cartify.controllers;
 
+import com.projects.cartify.exceptions.AlreadyExistException;
+import com.projects.cartify.exceptions.ResourceNotFoundException;
+import com.projects.cartify.models.Category;
+import com.projects.cartify.responses.ApiResponse;
+import com.projects.cartify.services.category.ICategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/categories")
 public class CategoryController {
+
+    private final ICategoryService categoryService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllCategories() {
+        try {
+            List<Category> categories = categoryService.getAllCategories();
+            return ResponseEntity.ok(new ApiResponse("Categories:", categories));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Error", INTERNAL_SERVER_ERROR));
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addCategory(@RequestBody  Category name) {
+        try {
+            Category newCategory = categoryService.addCategory(name);
+            return ResponseEntity.ok(new ApiResponse("Category created successfully", newCategory));
+        } catch (AlreadyExistException e) {
+            return ResponseEntity.status(CONFLICT).body(new ApiResponse("Category already exist", CONFLICT));
+        }
+    }
+
+    @GetMapping("/category/{id}")
+    public ResponseEntity<ApiResponse> getCategoryById(@PathVariable Long id) {
+       try {
+           Category category = categoryService.getCategoryById(id);
+           return ResponseEntity.ok(new ApiResponse("Category found", category));
+       } catch (ResourceNotFoundException e) {
+           return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), NOT_FOUND));
+       }
+    }
+
+    @GetMapping("/category/{name}")
+    public ResponseEntity<ApiResponse> getCategoryByName(@PathVariable String name) {
+        try {
+            Category category = categoryService.getCategoryByName(name);
+            return ResponseEntity.ok(new ApiResponse("Category found", category));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), NOT_FOUND));
+        }
+    }
+    @GetMapping("/delete/{id}")
+    public ResponseEntity<ApiResponse> deleteCategory(@PathVariable Long id) {
+        try {
+            categoryService.deleteCategoryById(id);
+            return ResponseEntity.ok(new ApiResponse("Category deleted successfully", null));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), NOT_FOUND));
+        }
+    }
+
 }
--- src/main/java/com/projects/cartify/controllers/ImageController.java
+++ src/main/java/com/projects/cartify/controllers/ImageController.java
@@ -1,11 +1,14 @@
 package com.projects.cartify.controllers;
 
 import com.projects.cartify.dto.ImageDto;
+import com.projects.cartify.exceptions.ResourceNotFoundException;
 import com.projects.cartify.models.Image;
 import com.projects.cartify.responses.ApiResponse;
 import com.projects.cartify.services.images.IImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +17,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.sql.SQLException;
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @RequiredArgsConstructor
 @RestController
@@ -28,13 +33,48 @@ public class ImageController {
             List<ImageDto> imageDtos = imageService.saveImages(files, id);
             return ResponseEntity.ok(new ApiResponse("upload successful", imageDtos));
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("Upload failed", e.getMessage()));
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Upload failed", e.getMessage()));
         }
     }
 
-    public ResponseEntity<ApiResponse> downloadImage(@PathVariable Long imageId) throws SQLException {
+
+    @GetMapping("/image/download/{imageId}")
+    public ResponseEntity<Resource> downloadImage(@PathVariable Long imageId) throws SQLException {
         Image image = imageService.getImageById(imageId);
         ByteArrayResource resource = new ByteArrayResource(image.getImage().getBytes(1, (int)image.getImage().length()));
-        return ResponseEntity.ok().contentType(MediaType.parseMediaType(image.getFileType()))
+        return  ResponseEntity.ok().contentType(MediaType.parseMediaType(image.getFileType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename\"" + image.getFileName() + "\"")
+                .body(resource);
+    }
+
+    @PutMapping("/image/update/{imageId}")
+    public ResponseEntity<ApiResponse> updateImage(@PathVariable Long imageId, @RequestBody MultipartFile file) {
+        try {
+            Image image = imageService.getImageById(imageId);
+            if (image != null) {
+                imageService.updateImage(file, imageId);
+                return ResponseEntity.ok(new ApiResponse("update successful", null));
+            }
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ApiResponse(e.getMessage(), null));
+        }
+
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("update failed", INTERNAL_SERVER_ERROR));
+    }
+
+    @DeleteMapping("/image/delete/{imageId}")
+    public ResponseEntity<ApiResponse> deleteImage(@PathVariable Long imageId) {
+        try {
+            Image image = imageService.getImageById(imageId);
+            if (image != null) {
+                imageService.deleteImageById(imageId);
+                return ResponseEntity.ok(new ApiResponse("Delete successful", null));
+            }
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ApiResponse(e.getMessage(), null));
+        }
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("update failed", INTERNAL_SERVER_ERROR));
     }
 }
